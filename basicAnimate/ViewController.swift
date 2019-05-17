a40c7c220a758a4cae4b09e853c9ae472f42342e
tjoa ay hok <ayhok2426@gmail.com>
2019-05-17 08:53:54 +0700

    Initial Commit

---
+++ basicAnimate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  basicAnimate
+//
+//  Created by tjoa ay hok on 17/05/19.
+//  Copyright © 2019 ayhok2426. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
